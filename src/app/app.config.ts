af3459360f3e7d35514d4fdab51da89b518baa4a
Mostafa Hossameldin <mahossameldin@gmail.com>
2024-12-13 16:31:34 +0200

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +14,17 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'top',
       })
     ),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'mini-elms-hci',
+        appId: '1:326813145960:web:35d5bab3f496d25d5754dd',
+        storageBucket: 'mini-elms-hci.firebasestorage.app',
+        apiKey: 'AIzaSyAPcwknbnR3dwbrq5SzvLbkAL6HL_jJYVA',
+        authDomain: 'mini-elms-hci.firebaseapp.com',
+        messagingSenderId: '326813145960',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
